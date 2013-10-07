354a6246ab46252aaa4f32d02fba770ea629bb0e
littlej <kokuahawaii@gmail.com>
2013-10-07 16:27:03 +0200

    Update metadata.php
    Translated fields: 'sitename_missing' & 'sitedescription_missing'
    
    'theme_settings' has been corrected (Apparence instead of Apperence)

--- fr_FR/metadata.php
+++ fr_FR/metadata.php
@@ -6,16 +6,16 @@ return array(
 	'metadata_desc' => 'Gérer les données du site',
 
 	'comment_settings' => 'Commentaires',
-	'theme_settings' => 'Apperence',
+	'theme_settings' => 'Apparence',
 
 	// form fields
 	'sitename' => 'Nom du site ',
 	'sitename_explain' => '',
-	'sitename_missing' => 'Your site needs a name!',
+	'sitename_missing' => 'Votre site requiert un nom',
 
 	'sitedescription' => 'Description du site ',
 	'sitedescription_explain' => '',
-	'sitedescription_missing' => 'Your site needs a description!',
+	'sitedescription_missing' => 'Votre site requiert une description',
 
 	'homepage' => 'Page d\'accueil ',
 	'homepage_explain' => '',
@@ -32,7 +32,7 @@ return array(
 	'comment_notifications' => 'Notifier les commentaires par e-mail ',
 	'comment_notifications_explain' => '',
 
-	'comment_moderation_keys' => 'Spam (mots interdits) ',
+	'comment_moderation_keys' => 'Filtre anti-spam (mots interdits) ',
 	'comment_moderation_keys_explain' => 'Liste séparée par une virgule de mots-clé identifiables comme étant du spam.
 		Les commentaires seront automatiquement notifiés comme du spam.',
 
